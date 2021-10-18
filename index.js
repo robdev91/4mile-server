181bb1951848e788117733159aa34b2a6f97df89
elif-bunny <robdev91@gmail.com>
2021-10-18 10:11:17 -0400

    change mock data

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const port = 3000
+
+app.use(cors())
 
 const repos = require('./mock').map(repo => {
   const { name, language, star_count } = repo
